645257d29a9c810f5559bc07b563c58dc61446c2
USERSAD\smunozla <sandra.munoz.lara@everis.com>
2022-03-01 20:35:39 +0100

    Add form player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators, FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit{
+
+    //Form
+    formNewUser: FormGroup;
+
+ 
+    //Constructor
+  constructor(fb: FormBuilder  
+  ){this.formNewUser = fb.group(
+    {
+     ["nameNewUser"]: ["", [Validators.required]]
+    },{}
+  );}
+
+
+
+  ngOnInit() {
+
+  }
+
+ 
 }
