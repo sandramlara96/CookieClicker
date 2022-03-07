6fd4bcc3af411f5ba282da6cee6691a5cfb90bb7
USERSAD\smunozla <sandra.munoz.lara@everis.com>
2022-03-07 11:47:36 +0100

    Fix

--- src/app/core/ranking/ranking.component.ts
+++ src/app/core/ranking/ranking.component.ts
@@ -29,12 +29,5 @@ export class RankingComponent implements OnInit {
     this.ranking = JSON.parse(localStorage.getItem("ranking"));
   }
 
-  ngAfterViewInit() {
-    if (!localStorage.getItem('foo')) {
-      localStorage.setItem('foo', 'no reload')
-      location.reload();
-    } else {
-      localStorage.removeItem('foo')
-    }
-  }
+
 }
